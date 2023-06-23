cd5ae42f57e2b5cce4f531d4a0f8ad5c951f3e35
ABDELKODOUSS BELHAIMER <74858385+BELHAIMER-ABDELKODOUSS@users.noreply.github.com>
2023-06-23 17:33:33 +0100

    add sleeps

--- jobs.py
+++ jobs.py
@@ -10,12 +10,12 @@ def txt2img(request: dict):
         requests.post(url=f'{url}/sdapi/v1/txt2img', json=request, timeout=5)
     except Exception as e:
         pass
-
+    
     response = requests.get(url=f'{url}/sdapi/v1/progress?skip_current_image=true')
     while response.json()["progress"] != 0:
         time.sleep(3)
         response = requests.get(url=f'{url}/sdapi/v1/progress?skip_current_image=true')
-
+    time.sleep(5)
     images_out = []
     for i, image_base64 in enumerate(response.json()['current_processed_images']):
         images_out.append(image_base64)
@@ -32,5 +32,5 @@ def upscale(request: dict):
     while response.json()["progress"] != 0:
         time.sleep(3)
         response = requests.get(url=f'{url}/sdapi/v1/progress?skip_current_image=true')
-
+    time.sleep(5)
     return {"image": response.json()['current_processed_images'][0]}
